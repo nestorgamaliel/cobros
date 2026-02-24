cc9f945b98e9dfbb65d1e8c177d9637684193e08
Nestor Gonzalez <nestorgamaliel@gmail.com>
2026-02-24 15:48:30 -0600

    Correccion para refactorar Pydentic

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-from app.models.models import Base, Persona, Credito, Pago, Vendedor
+from .models import Base, Persona, Credito, Pago, Vendedor, CreditoComision
 
-__all__ = ['Base', 'Persona', 'Credito', 'Pago', 'Vendedor']
+__all__ = ['Base', 'Persona', 'Credito', 'Pago', 'Vendedor', 'CreditoComision']
