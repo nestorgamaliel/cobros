3ec2436194191ff9c99565fa1bd43085d88caaee
Nestor Gonzalez <nestorgamaliel@gmail.com>
2026-08-08 12:21:24 -0600

    Fix - Reestructuracion - 2

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,21 @@
-from .models import Base, Persona, Credito, Pago, Vendedor, CreditoComision
+from .models import (
+    Base, 
+    Persona, 
+    Credito, 
+    Pago, 
+    Vendedor, 
+    CreditoComision,
+    CreditoReestructuracion,
+    CreditoReestructuracionDetalle
+)
 
-__all__ = ['Base', 'Persona', 'Credito', 'Pago', 'Vendedor', 'CreditoComision']
+__all__ = [
+    'Base', 
+    'Persona', 
+    'Credito', 
+    'Pago', 
+    'Vendedor', 
+    'CreditoComision',
+    'CreditoReestructuracion',
+    'CreditoReestructuracionDetalle'
+]
